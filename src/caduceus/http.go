6b0dc46d25406300a3b6b6769bed27fbc9f8bc54
Holaday, Sean <Sean_Holaday@comcast.com>
2017-03-02 09:52:15 -0800

    Updating the code to pull the entire contents of the payload rather than decode parts of it into JSON.

--- src/caduceus/http.go
+++ src/caduceus/http.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
 	"fmt"
 	"net/http"
 )
 
 func (sh *ServerHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	defer request.Body.Close()
-
 	sh.logger.Info("Someone is saying hello!")
 	fmt.Fprintf(response, "%s", []byte("Heyo whaddup!\n"))
 
-	var myPayload JSONPayload
-	err := json.NewDecoder(request.Body).Decode(&myPayload)
-	if err != nil {
-		sh.logger.Info("Failed to parse the payload: %s", err.Error())
-		fmt.Fprintf(response, "%s", []byte("Payload failed to parse!\n"))
-	}
+	myBuf := new(bytes.Buffer)
+	myBuf.ReadFrom(request.Body)
+	myPayload := myBuf.Bytes()
 
 	sh.workerPool.Send(func(workerID int) { sh.HandleRequest(workerID, myPayload) })
+	request.Body.Close()
 }
 
-func (sh *ServerHandler) HandleRequest(workerID int, inPayload JSONPayload) {
-	sh.logger.Info("Worker ID #%d is printing the payload: %s", workerID, inPayload.Payload)
+func (sh *ServerHandler) HandleRequest(workerID int, inPayload []byte) {
+	sh.logger.Info("Worker ID #%d is handling a request...", workerID)
+
+	sh.logger.Info("Worker ID #%d is printing the payload: %s", workerID, string(inPayload))
 }
