ac00815c0cb12dffdc4e1f3fdd8984fda0658de3
Holaday, Sean <Sean_Holaday@comcast.com>
2017-03-03 16:47:56 -0800

    Starting to get testing functions for caduceus up and running!

---
+++ src/caduceus/caduceus_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/Comcast/webpa-common/health"
+	"github.com/Comcast/webpa-common/logging"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+// Begin mock declarations
+type mockHandler struct {
+	mock.Mock
+}
+
+func (m *mockHandler) HandleRequest(workerID int, inRequest CaduceusRequest) {
+	return
+}
+
+type mockHealthTracker struct {
+	mock.Mock
+}
+
+func (m *mockHealthTracker) Increment(inStat health.Stat) {
+	return
+}
+
+func (m *mockHealthTracker) IncrementBucket(inSize int) {
+	return
+}
+
+// Begin test functions
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestServeHTTP(t *testing.T) {
+	assert := assert.New(t)
+
+	logger := &logging.LoggerWriter{ioutil.Discard}
+	fakeHandler := &mockHandler{}
+	fakeHealth := &mockHealthTracker{}
+
+	serverWrapper := &ServerHandler{
+		logger:          logger,
+		caduceusHandler: fakeHandler,
+		caduceusHealth:  fakeHealth,
+		workerPool: WorkerPoolFactory{
+			NumWorkers: 1,
+			QueueSize:  1,
+		}.New(),
+	}
+
+	testServer := httptest.NewServer(serverWrapper)
+	defer testServer.Close()
+
+	buf := bytes.NewBufferString("Test message.")
+
+	res, err := http.Post(testServer.URL, "text/plain", buf)
+	assert.Nil(err)
+
+	resMsg, err := ioutil.ReadAll(res.Body)
+	assert.Nil(err)
+
+	fmt.Println(string(resMsg))
+
+	res.Body.Close()
+
+	fakeHandler.AssertExpectations(t)
+	fakeHealth.AssertExpectations(t)
+}
