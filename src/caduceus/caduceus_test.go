d86226a8aece802f82cfe5e117d3b4077f847ec6
Holaday, Sean <Sean_Holaday@comcast.com>
2017-03-10 13:29:23 -0800

    Reorganizing ServeHTTP method tests.

--- src/caduceus/caduceus_test.go
+++ src/caduceus/caduceus_test.go
@@ -57,68 +57,56 @@ func TestServeHTTP(t *testing.T) {
 		doJob:           workerPool.Send,
 	}
 
+	req := httptest.NewRequest("POST", "localhost:8080", strings.NewReader("Test payload."))
+
 	t.Run("TestHappyPath", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "localhost:8080", strings.NewReader("Test payload."))
 		req.Header.Set("Content-Type", "text/plain")
+
 		w := httptest.NewRecorder()
-
 		serverWrapper.ServeHTTP(w, req)
-
 		resp := w.Result()
 
 		assert.Equal(202, resp.StatusCode)
-
-		fakeHandler.AssertExpectations(t)
-		fakeHealth.AssertExpectations(t)
-	})
-
-	t.Run("TestWrongHeader", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "localhost:8080", strings.NewReader("Test payload."))
-		req.Header.Set("Bad-Header", "not/real")
-		w := httptest.NewRecorder()
-
-		serverWrapper.ServeHTTP(w, req)
-
-		resp := w.Result()
-
-		assert.Equal(400, resp.StatusCode)
-
 		fakeHandler.AssertExpectations(t)
 		fakeHealth.AssertExpectations(t)
 	})
 
 	t.Run("TestTooManyHeaders", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "localhost:8080", strings.NewReader("Test payload."))
-		req.Header.Set("Content-Type", "text/plain")
 		req.Header.Add("Content-Type", "too/many/headers")
+
 		w := httptest.NewRecorder()
-
 		serverWrapper.ServeHTTP(w, req)
-
 		resp := w.Result()
 
 		assert.Equal(400, resp.StatusCode)
+		fakeHandler.AssertExpectations(t)
+		fakeHealth.AssertExpectations(t)
+	})
 
+	t.Run("TestWrongHeader", func(t *testing.T) {
+		req.Header.Del("Content-Type")
+
+		w := httptest.NewRecorder()
+		serverWrapper.ServeHTTP(w, req)
+		resp := w.Result()
+
+		assert.Equal(400, resp.StatusCode)
 		fakeHandler.AssertExpectations(t)
 		fakeHealth.AssertExpectations(t)
 	})
 
 	t.Run("TestFullQueue", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "localhost:8080", strings.NewReader("Test payload."))
 		req.Header.Set("Content-Type", "text/plain")
-		w := httptest.NewRecorder()
 
+		w := httptest.NewRecorder()
 		requestTimeout := func(func(workerID int)) error {
 			return errors.New("Intentional error.")
 		}
-
 		serverWrapper.doJob = requestTimeout
 		serverWrapper.ServeHTTP(w, req)
-
 		resp := w.Result()
 
 		assert.Equal(408, resp.StatusCode)
-
 		fakeHandler.AssertExpectations(t)
 		fakeHealth.AssertExpectations(t)
 	})
