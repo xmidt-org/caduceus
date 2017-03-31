8da32a718d55ea110db40081a873be568bb1a9b1
Weston Schmidt <Weston_Schmidt@cable.comcast.com>
2017-03-31 01:44:28 -0700

    Remove the alpha from the name as it will cause CD issues.

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,35 +4,19 @@ NAME=caduceus
 
 echo "Adjusting build number..."
 
-OIFS=$IFS
-IFS='
-
-'
-
-release=""
-
 taglist=`git tag -l`
 tags=($taglist)
 
-for ((i=${#tags[@]}-1; i >=0; i--)); do
-    if [[ "${tags[i]}" != *"alpha"* ]]; then
-        release=${tags[i]}
-        break
-    fi
-done
+release=${tags[${#tags[@]}-1]}
 
 if [ -z "$release"  ]; then
     echo "Could not find latest release tag!"
 else
     echo "Most recent release tag: $release"
 fi
+new_release=`echo "$release" | sed -e 's/-\d*.*//'`
+new_release+="-${BUILD_NUMBER}"
 
-IFS=$OIFS
-
-release=`echo "$release" | awk -F. '{$NF+=1; OFS="."; print $0}'`
-new_release=$release
-new_release+="-${BUILD_NUMBER}alpha"
-release=`echo "$release" | awk -F'v' '{print $2}'`
 echo "Issuing release $new_release..."
 echo "New base version: $release..."
 
