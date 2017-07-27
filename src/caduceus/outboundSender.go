be81826c4b64ed37722e9e24bce5ced7bf4b1051
Weston Schmidt <Weston_Schmidt@cable.comcast.com>
2017-07-27 01:46:36 -0700

    Revert undesireable log entry.

--- src/caduceus/outboundSender.go
+++ src/caduceus/outboundSender.go
@@ -390,7 +390,6 @@ func (obs *CaduceusOutboundSender) worker(id int) {
 				} else {
 					if (200 <= resp.StatusCode) && (resp.StatusCode <= 204) {
 						// Report success
-						obs.logger.Error("Success sending to '%s'", obs.listener.Config.URL)
 						obs.profiler.Send(work.req)
 					} else {
 						// Report partial success
