45fc12d95b13f86c0644d6e8f213b45c292bab30
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2019-09-03 14:16:24 -0700

    The change should have just been a patch change.

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ import (
 const (
 	applicationName    = "caduceus"
 	DEFAULT_KEY_ID     = "current"
-	applicationVersion = "0.3.1"
+	applicationVersion = "0.2.1"
 )
 
 // caduceus is the driver function for Caduceus.  It performs everything main() would do,
