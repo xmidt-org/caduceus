7fd77d6049f2723ea1167b54caea866011ee2560
maura fortino <maurafortino@gmail.com>
2023-12-05 11:26:40 -0500

    added servers to caduceus func

--- main.go
+++ main.go
@@ -130,6 +130,12 @@ func caduceus(arguments []string, run bool) error {
 		provideMetricEndpoint(),
 		provideHealthCheck(),
 
+		arrangehttp.ProvideServer("servers.health"),
+		arrangehttp.ProvideServer("servers.metrics"),
+		arrangehttp.ProvideServer("servers.pprof"),
+		// arrangehttp.ProvideServer("servers.primary"),
+		// arrangehttp.ProvideServer("servers.alternate"),
+
 		touchstone.Provide(),
 		touchhttp.Provide(),
 		ProvideMetrics(),
