164f14ac6a23580c880e2a15675d8442801a4d16
maura fortino <maurafortino@gmail.com>
2025-04-11 12:32:19 -0400

    added copyright for mocks

--- internal/handler/mock_test.go
+++ internal/handler/mock_test.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+// SPDX-FileCopyrightText: 2025 Comcast Cable Communications Management, LLC
 // SPDX-License-Identifier: Apache-2.0
 package handler
 
