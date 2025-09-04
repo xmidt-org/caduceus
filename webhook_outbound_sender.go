552e98080e4d2f4c2b578f832ea305784868655e
mpicci200_comcast <piccione99@yahoo.com>
2025-09-04 13:17:13 -0400

    switch internal license out

--- webhook_outbound_sender.go
+++ webhook_outbound_sender.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+// SPDX-FileCopyrightText: 2023 Comcast Cable Communications Management, LLC
 // SPDX-License-Identifier: Apache-2.0
 
 package main
